a0abe8e242fd8a9975c4a58ae721f3a6aac65831
Aditya Aware <101728767+Aditya-Aware@users.noreply.github.com>
2024-10-07 14:11:55 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -11,7 +11,11 @@ class Solution(object):
         for char in s: 
             if char in pairs.values():
                 stack.append(char)
-            
+            elif char in pairs.keys():
+                if not stack or pairs[char] != stack.pop():
+                    return False
+            else:
+                return False
         pass
 
 
