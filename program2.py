604deb5bdae75d657d4a873c1227fa871e084e00
Aditya Aware <101728767+Aditya-Aware@users.noreply.github.com>
2024-10-07 14:17:08 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -6,7 +6,13 @@ class Solution(object):
         """
 
         roman_map= {
-            
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
         }
 
         pass
